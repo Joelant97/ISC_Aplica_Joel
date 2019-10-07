e52125c4ad69ebddac03874459524157e2b740ca
Joel Antonio <joelant97@gmail.com>
2019-10-07 06:10:07 -0400

    Update

---
+++ SpaceProyectoFinal/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame(Esto es del Pong Player Bihaviour)
+	void Update () {
+        if (i_cpuPlayer || _is)
+        {
+            _deltaPos = new Vector3(0, _movementSpeed = Input.GetAxis(_isPlayerOne ? "player1" : "player2"));
+            transform.Translate_
+        }
+    
+	}
+}
+ 
--- Tictatoe/Tictatoe/Program.cs
+++ Tictatoe/Tictatoe/Program.cs
@@ -17,31 +17,48 @@ namespace Tictatoe
 
     public class Game
     {
+        public int PlayerTurn { get; set; }
         public GameState CurrentState { get; set; }
         public char[,] Board { get; set; }
         public bool CurrentPlayer { get; set; }
+        
+    }
+
+    public enum GameOverState
+    {
+        GameOn,
+        Player1,
+        Player2,
+        Tie
     }
 
     class Program
     {
-        const int LEFTMARGIN = 4;
+        const int LEFTMARGIN = 4, COLSIZE = 10, ROWSIZE = 4;
+        static int tmp;
         const int TOPMARGIN = 2;
         const int FPSTIME = 33;
         public static Game CurrentGame;
+        static char tmpPlayer;
+
         static Thread _inputThread;
+
 
         public static void InitializeGame()
         {
             CurrentGame = new Game();
-            CurrentGame.Board = new short[3, 3];
+            CurrentGame.Board = new char[3, 3];
             CurrentGame.CurrentState = GameState.Menu;
+            CurrentGame.PlayerTurn = 0;
             _inputThread = new Thread(GetInput);
             _inputThread.Start();
+
         }
 
         public static void Main(string[] args)
         {
             InitializeGame();
+            char prueba;
             while (CurrentGame.CurrentState != GameState.Gameover)
             {
                 switch (CurrentGame.CurrentState)
@@ -55,6 +72,22 @@ namespace Tictatoe
                     case GameState.Playing:
                         ShowFrame(false);
                         ShowBoard();
+                        CurrentGame.CurrentState = GameState.Gameover;
+                        switch (CheckGameOver())
+                        {
+                            case GameOverState.Player1:
+                                Console.WriteLine("Ha ganado el jugador 1!");
+                                break;
+                            case GameOverState.Player2:
+                                Console.WriteLine("Ha ganado el jugador 2!");
+                                break;
+                            case GameOverState.Tie:
+                                Console.WriteLine("Empate!");
+                                break;
+                            default:
+                                CurrentGame.CurrentState = GameState.Playing;
+                                break;
+                        }
                         break;
 
                 }
@@ -67,7 +100,7 @@ namespace Tictatoe
         static void ShowFrame(bool isStarting)
         {
             Console.Clear();
-            Console.WriteLine("#################");
+            //Console.WriteLine("#################");
             Console.WriteLine("  7  |  8  |  9  ");
 
             Console.WriteLine("     |     |     ");
@@ -85,7 +118,7 @@ namespace Tictatoe
             Console.WriteLine("     |     |     ");
 
             Console.WriteLine("     |     |     ");
-            Console.WriteLine("#################");
+           // Console.WriteLine("#################");
             //Console.WriteLine($"\n{(isStarting ? "Presione una tecla para empezar con el jugador 1." : ("Le toca al jugador " + CurrentGame.CurrentPlayer ? "dos. : "uno. ")))}");
             Console.Write("Seleccione una casilla [1-9]");
         }
@@ -104,21 +137,51 @@ namespace Tictatoe
         static void ShowMenu()
         {
             Console.Clear();
-            Console.WriteLine("Hola! vamo a juga' tic tac toe...");
+            Console.WriteLine("Hola! vamo a jugar tic tac toe...");
             Console.Write("\nSeleccione una opción:");
             Console.WriteLine("\n\t1: Jugar");
             Console.WriteLine("\t2: Salir");
             Console.WriteLine("\n\tSeleccione: ");
         }
 
+        //Retorna el ganador del Juego(Comportamientos).
+        static GameOverState CheckGameOver()
+        {
+            
+            tmpPlayer = '\0';
+            for (tmp = 0; tmp < 3; tmp++)
+            {
+                if (CurrentGame.Board[tmp, 0] != '\0' && CurrentGame.Board[tmp, 0] == CurrentGame.Board[tmp, 1] && CurrentGame.Board[tmp, 0] == CurrentGame.Board[tmp, 2])
+                    tmpPlayer = CurrentGame.Board[tmp, 0];
+                if (CurrentGame.Board[0, tmp] != '\0' && CurrentGame.Board[0, tmp] == CurrentGame.Board[1, tmp] && CurrentGame.Board[0, tmp] == CurrentGame.Board[2, tmp])
+                    tmpPlayer = CurrentGame.Board[0, tmp];
+            }
+            if (CurrentGame.Board[0, 0] != '\0' && CurrentGame.Board[0, 0] == CurrentGame.Board[1, 1] && CurrentGame.Board[0, 0] == CurrentGame.Board[2, 2])
+                tmpPlayer = CurrentGame.Board[0, 0];
+            if (CurrentGame.Board[0, 2] != '\0' && CurrentGame.Board[0, 2] == CurrentGame.Board[1, 1] && CurrentGame.Board[0, 2] == CurrentGame.Board[2, 0])
+                tmpPlayer = CurrentGame.Board[0, 2];
+
+            if (tmpPlayer != '\0')
+                return tmpPlayer == 'X' ? GameOverState.Player1 : GameOverState.Player2;
+            //Caso de empate.
+            if (CurrentGame.PlayerTurn == CurrentGame.Board.Length)
+                return GameOverState.Tie;
+            //Juego sigue hasta llenar el tablero o Gane un Jugador. 
+            return GameOverState.GameOn;
+        }
+
+
         static void GetInput()
         {
+            //El input hace el trabajo de capturar para luego actualizar el comportamientos.
             string _currentInput;
+            int i, j;
             while (true)
             {
                 switch (CurrentGame.CurrentState)
                 {
                     case GameState.Menu:
+                        _currentInput = Console.ReadKey().KeyChar.ToString();
                         _currentInput = Console.ReadKey().KeyChar.ToString();
                         CurrentGame.CurrentState = _currentInput == "1" ? GameState.Starting : GameState.Gameover;
                         break;
@@ -128,7 +191,21 @@ namespace Tictatoe
                         break;
                     case GameState.Playing:
                         _currentInput = Console.ReadKey().KeyChar.ToString();
-                        CurrentGame.Board[] = CurrentGame.CurrentPlayer ? 'O' : 'x';
+                        tmp = Convert.ToInt32(_currentInput);
+
+                        if (tmp < 1 || tmp > 9)
+                            continue;
+
+                        i = 3 - ((tmp - 1) / 3) - 1;
+                        j = (tmp - 1) % 3;
+
+                        if (CurrentGame.Board[i, j] != '\0')
+                            continue;
+
+                        CurrentGame.PlayerTurn++;
+
+                        CurrentGame.Board[i, j] = CurrentGame.CurrentPlayer ? 'O' : 'x';
+                        CurrentGame.CurrentPlayer = !CurrentGame.CurrentPlayer;
                         break;
                 }
             }
