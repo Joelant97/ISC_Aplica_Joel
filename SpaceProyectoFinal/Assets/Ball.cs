fe69d348ebbbfd4853b3519bf5ad14f50f48c51e
Joelant97 <joelant97@gmail.com>
2019-10-14 02:50:07 +0000

    add sound

--- SpaceProyectoFinal/Assets/Ball.cs
+++ SpaceProyectoFinal/Assets/Ball.cs
@@ -20,21 +20,25 @@ public class Ball : MonoBehaviour {
 	void Update () {
         transform.Translate(dir * speed * Time.deltaTime);
 
+        //Laterales Horizontales
         if (transform.position.y < GameManager.bottomLeft.y + radio && dir.y < 0)
         {
             dir.y = -dir.y;
+            //ScoreJugador.instance.GiveJugadorUnoPuntos();
         }
 
         if (transform.position.y > GameManager.topRight.y - radio && dir.y > 0)
         {
             dir.y = -dir.y;
+            //ScoreJugador.instance.GiveJugadorDosPuntos();
         }
 
 
-        //Fin del Juego
+        //Suma de Puntos (DeadZone)
         if (transform.position.x < GameManager.bottomLeft.x + radio && dir.x < 0)
         {
-            Debug.Log("Jugador1 (Der) ha ganado. ");
+    
+            Debug.Log("Jugador1 (Der.) a ganado!");
 
             Time.timeScale = 0;
             enabled = false;
@@ -42,14 +46,14 @@ public class Ball : MonoBehaviour {
 
         if (transform.position.x > GameManager.topRight.x - radio && dir.x > 0)
         {
-            Debug.Log("Jugador2 (Izq) ha ganado. ");
-            Time.timeScale = 0;
+            Debug.Log("Jugador2 (Izq.) a ganado!");
 
+            Time.timeScale = 0;
             enabled = false;
         }
 
     }
-
+    //OnTrigger de la Barra jugador.
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Jugador")
@@ -59,10 +63,14 @@ public class Ball : MonoBehaviour {
             if (isRight == true && dir.x > 0)
             {
                 dir.x = -dir.x;
+                //Contar Puntos
+                ScoreJugador.instance.GiveJugadorUnoPuntos();
             }
             if (isRight == false && dir.x < 0)
             {
                 dir.x = -dir.x;
+                //Contar Puntos
+                ScoreJugador.instance.GiveJugadorDosPuntos();
             }
         }
     }
---
+++ SpaceProyectoFinal/Assets/ScoreJugador.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreJugador : MonoBehaviour {
+    public static ScoreJugador instance;
+
+    public Text JugadorUnoScoreText;
+    public Text JugadorDosScoreText;
+    
+    public int JugadorUnoScore;
+    public int JugadorDosScore;
+
+
+	// Use this for initialization
+	void Start () {
+        instance = this;
+
+        JugadorUnoScore = JugadorDosScore = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GiveJugadorUnoPuntos() {
+        JugadorUnoScore += 1;
+        JugadorUnoScoreText.text = JugadorUnoScore.ToString();
+
+    }
+
+    public void GiveJugadorDosPuntos()
+    {
+        JugadorDosScore += 1;
+        JugadorDosScoreText.text = JugadorDosScore.ToString();
+
+    }
+
+}
